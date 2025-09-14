c55127ddc906e6eddea1ccdc85ef13eb4da94752
Yurden <blinddude@yandex.ru>
2025-09-14 21:25:20 +0300

    Теперь сайт не реагирует на запросы, пока пользователь не авторизуется.

---
+++ main_section/middleware.py
@@ -0,0 +1,23 @@
+# middleware.py
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class AuthRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # URL, которые доступны без авторизации
+        public_urls = [
+            reverse('login'),
+            reverse('register'),
+            reverse('logout'),
+            '/admin/login/',
+            # добавьте другие публичные URL
+        ]
+
+        if not request.user.is_authenticated and request.path not in public_urls:
+            return redirect('login')
+
+        return self.get_response(request)
