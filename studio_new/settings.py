bf6c9468d6deb93d241df295bf552f20c471ec76
Yurden-MSKV <blinddude@yandex.ru>
2025-09-23 14:02:00 +0300

    Добавил ограничение для единовременной загрузки файлов.

--- studio_new/settings.py
+++ studio_new/settings.py
@@ -168,3 +168,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
