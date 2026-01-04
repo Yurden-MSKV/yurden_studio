bfb054968f9bb102e5e5ff7159c2d2c0281d537f
Yurden <blinddude@yandex.ru>
2026-01-04 16:22:49 +0300

    Обновил вывод User в админке.

--- main_section/admin.py
+++ main_section/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from main_section.models import ChapterLike
+
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ['username', 'date_joined']
+    sortable_by = ('date_joined',)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 
 @admin.register(ChapterLike)
