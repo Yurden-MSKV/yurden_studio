0103a660371586e4f914bb9aa52d69216e740e61
Yurden <blinddude@yandex.ru>
2026-01-04 16:41:54 +0300

    Добавил вывод chapterview и сортировку по дате регистрации.

--- main_section/admin.py
+++ main_section/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from main_section.models import ChapterLike
+from main_section.models import ChapterLike, ChapterView
+from manga_section.models import Chapter
+
 
 class CustomUserAdmin(BaseUserAdmin):
     list_display = ['username', 'date_joined']
-    sortable_by = ('date_joined',)
+    # sortable_by = ('date_joined',)
+    ordering = ('-date_joined',)
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
@@ -15,3 +18,7 @@ admin.site.register(User, CustomUserAdmin)
 @admin.register(ChapterLike)
 class ChapterLikeAdmin(admin.ModelAdmin):
     list_display = ['user', 'chapter', 'is_like']
+
+@admin.register(ChapterView)
+class ChapterViewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'manga', 'chapter', 'view_date']
